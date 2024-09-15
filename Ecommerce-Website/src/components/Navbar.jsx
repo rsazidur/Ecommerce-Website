a59b1348717c4b1a29cac58f4bbdd5f34041881c
rsazidur <rsazidur936@gmail.com>
2024-09-15 14:48:27 +0600

    Mobile Navbar Added

--- Ecommerce-Website/src/components/Navbar.jsx
+++ Ecommerce-Website/src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import { FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
+import React, { useState }from 'react'
+import { FaBars, FaTimes, FaSearch, FaShoppingBag, FaUser } from "react-icons/fa";
 import logo from '/logo.png'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     const navItems = [
         {title: 'Shooting Games', path: '/'},
@@ -23,9 +29,18 @@ const Navbar = () => {
         <a href="/"><img src={logo} alt=""/></a>
 
         {/* account and shoping btn */}
-        <div className='text-lg text-Black sm:flex items-center gap-6 hidden '>
+        <div className='text-lg text-Black sm:flex items-center gap-4 hidden '>
           <a href='/' className='flex items-center gap-2'><FaUser /> Account</a>
           <a href='/' className='flex items-center gap-2'><FaShoppingBag /> Shopping</a>
+        </div>
+
+        {/* Mobile Menu */}
+        <div className='sm:hidden'>
+          <button onClick={toggleMenu}>
+            {
+            isMenuOpen ? <FaTimes className="w-5 h-5 text-Black"/> : <FaBars className="w-5 h-5 text-Black"/>
+            }
+          </button>
         </div>
       </nav>
 
@@ -43,8 +58,22 @@ const Navbar = () => {
             }
         </ul>
       </div>
+        
+      {/*Only Mobile Menu */}
+      <div>
+      <ul className={`bg-Black text-white px-4 py-2 rounded ${isMenuOpen ? "" : "hidden"}`}>
+            {
+                navItems.map(({title, path}) => (
+                    <li key={title} className='hover:text-rose-600 my-3 cursor-pointer'>
+                        <Link to={"/"}>{title}</Link>
+                    </li>
+                ))
+            }
+        </ul>
+      </div>
+
     </header>
   )
 }
 
-export default Navbar 
+export default Navbar
